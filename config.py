96d0c2ee21c05d7450ea72767d238e3c662efce0
e <17615800805@163.com>
2021-07-29 10:12:04 +0800

    logger fix

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 from loguru import logger
 
 logger.add('./Logs/Runtime.log', level='DEBUG', retention='7 days', encoding='utf-8')
-logger.add('./Logs/OOCLError.log', level='ERROR', retention='7 days', encoding='utf-8')
+logger.add('./Logs/Error.log', level='ERROR', retention='7 days', encoding='utf-8')
 
 # api配置
 API_HOST = '0.0.0.0'
